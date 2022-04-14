9f46ebfd70aa563ce85177356fb50bb01c799135
Alexandre Nazato <a.nazato@gmail.com>
2022-04-14 09:32:26 -0300

    Primeira aula java

---
+++ src/aula01.java
@@ -0,0 +1,13 @@
+
+public class aula01 {
+
+	public aula01() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		
+	}
+
+}
